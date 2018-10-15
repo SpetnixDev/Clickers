f17ad6a9e633a9a142417f18fc42d3057220ecf2
Spetnix <spetnix@gmail.com>
2018-10-16 01:52:01 +0200

    Added new class Test

--- src/net/spetnix/Main.java
+++ src/net/spetnix/Main.java
@@ -1,8 +1,8 @@
 package net.spetnix;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Test test = new Test(5);
+        test.display();
     }
 }
---
+++ src/net/spetnix/Test.java
@@ -0,0 +1,13 @@
+package net.spetnix;
+
+public class Test {
+    private int value;
+
+    public Test(int value) {
+        this.value = value;
+    }
+
+    public void display() {
+        System.out.println(value);
+    }
+}
